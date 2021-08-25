359f9eb8449383e24c54462499427684770c289d
Andres Gomez <anfegor@hotmail.com>
2021-08-24 23:04:37 -0500

    lot of changes on all the code, now it works with JS giving all the coupons

--- docs/discounts.js
+++ docs/discounts.js
@@ -1,37 +1,37 @@
-// const originalPrice = 120
-// const discount = 18
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajeDelPrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDelPrecioConDescuento)/ 100;
 
+    return precioConDescuento;
 
-
-// console.log({
-//     originalPrice,
-//     discount,
-//     percentPricesWithDiscount,
-//     priceWithDiscount,
-
-// });
-
-function calculatePriceWithDiscount (price  , discount) {
-
-    const percentPricesWithDiscount = 100 - discount;
-    const priceWithDiscount = (price * (percentPricesWithDiscount))/ 100
-
-    return priceWithDiscount;
 }
 
-function onClickPriceWithDiscount() {
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+    const resultCoupon = document.getElementById("priceWithCoupon")
+    let descuento;
+    let precioConDescuento;
 
-
-    const InputPrice = document.getElementById("InputPrice")
-    const priceValue = InputPrice.value;
-
-    const InputDiscount = document.getElementById("InputDiscount")
-    const discountValue = InputDiscount.value;
-
-    const priceWithDiscount = calculatePriceWithDiscount(priceValue, discountValue);
-
-    const priceResult = document.getElementById("priceResult")
-
-    priceResult.innerText = "The magic price is $" + priceWithDiscount;
+    switch(true) {
+        case couponValue === "Lucky":
+        descuento = 15;
+        precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+        resultCoupon.innerText =`It´s your lucky day, we´ve split the price on %${descuento} leaving the total to pay o : $${precioConDescuento} Dollars`
+        break;
+        case couponValue === "SayIt":
+        descuento = 30;
+        precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+        resultCoupon.innerText =`It´s your lucky day, we´ve split the price on %${descuento} leaving the total to pay o : $${precioConDescuento} Dollars`
+        break;
+        case couponValue === "Magic":
+        descuento = 25;
+        precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+        resultCoupon.innerText =`It´s your lucky day, we´ve split the price on % ${descuento} leaving the total to pay o : $${precioConDescuento}`
+        break;
+        default:
+        resultCoupon.innerText = "Are you trying to guess?"
+    }
 }
-
